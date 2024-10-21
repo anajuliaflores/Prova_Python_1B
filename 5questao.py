bd0470a7a383af5a96de804f1d755de18ba15bf4
anajuliaflores <160748504+anajuliaflores@users.noreply.github.com>
2024-10-21 08:57:51 -0300

    Add files via upload

---
+++ 5questao.py
@@ -0,0 +1,53 @@
+#Solução 1
+
+import time
+import random
+start = time.time()
+
+def tem_duplicados1(lista):
+    for i in range(len(lista)):
+        for j in range(i + 1, len(lista)):
+            if lista[i] == lista [j]:
+                return True
+    return False
+
+
+def tem_duplicados2(lista):
+    lista_ordenada = sorted(lista)
+    for i in range(len(lista_ordenada) - 1):
+        if lista_ordenada[i] == lista_ordenada[i + 1]:
+            return True
+    return False
+
+
+def tem_duplicados3(lista):
+    elementos_vistos = set()
+    for elemento in lista:
+      if elemento in elementos_vistos:
+        return True
+    elementos_vistos.add(elemento)
+    return False
+
+lista = random.sample(range(0,1000000), 20000)
+start = time.time()
+tem_duplicados1(lista)
+end = time.time()
+duration = end - start 
+
+print('Tempo de execução: %f' % duration)
+
+#_______________________________________________#
+
+start = time.time()
+tem_duplicados2(lista)
+end = time.time()
+duration = end - start 
+print('Tempo de execução: %f' % duration)
+
+#_______________________________________________#
+
+start = time.time()
+tem_duplicados3(lista)
+end = time.time()
+duration = end - start 
+print('Tempo de execução: %f' % duration)
