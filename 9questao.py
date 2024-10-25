75a8da43c4e56fa77cde0efa538b7f4094f0c205
anajuliaflores <160748504+anajuliaflores@users.noreply.github.com>
2024-10-25 10:08:03 -0300

    Add files via upload

--- 9questao.py
+++ 9questao.py
@@ -1,4 +1,4 @@
-alpha = float(input('Informealpha:'))
+alpha = float(input('Informe alpha:'))
 x = float(input('Informe x:'))
 if alpha > 0.1:
     print('Valor inválido para alpha')
@@ -7,3 +7,10 @@ else:
         print('aplpha * x')
     else:
             print(x)
+
+"""
+A respota seria sim, pois dadas as suposições em que a entrada de aplpha seria 2 e x seria 0 o retorno seria "Valor inválido para alpha". 
+Se em outra situação alpha fosse 0 e x 0 o retorno seria 0. E para alpha sendo -1 e x 3 o retorno seria 3.
+Assim sendo, o código funciona perfeitamente.
+Minha resposta foi uma resolução do que o código estava me mostrando e algumas suposições.
+"""
